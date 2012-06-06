9a1881b8f639a94b39465ddc14ac6ccf18534d73
Rafael Oshiro <rafael.oshiro@gmail.com>
2012-06-06 10:04:59 +0100

    Center the map in Dublin

--- public/js/itinerary.js
+++ public/js/itinerary.js
@@ -100,5 +100,60 @@ function(Y){
             node: v
         });
     });
-    	
+    
+	$('.delete-link').tooltip();
+	
+	/*
+	 *	Calculates directions on map.
+	 */
+	var io,delegate,routeController,
+		_drawRoute = function() {	
+		MQA.withModule('route','routeio', function() {
+			io = new MQA.RouteIO(MQROUTEURL,true);
+			delegate = new MQA.Route.RouteDelegate();
+
+			delegate.customizeRibbon = function(ribbonLineOverlay) {
+				ribbonLineOverlay.color = "#6C2DC7";
+				ribbonLineOverlay.fillColor = "#6C2DC7";
+				ribbonLineOverlay.colorAlpha = .8;
+				ribbonLineOverlay.borderWidth = 14;
+			};
+		
+			routeController = map.createRoute(delegate, io);
+			routeController.draggable = true;
+			routeController.poidrag = true;
+		
+			_route(io);
+		});
+	},
+	
+	_route = function(io, newLocations) {
+		var firstParam,
+			directionsLocations,
+			tmpMapState;
+
+		if(!newLocations) {
+			tmpMapState = routeController.delegate.virtualMapState(map);
+		} else {
+			tmpMapState = mapState;
+		}
+		
+		firstParam = {locations: [
+			{lat: 53.34804, lng: -6.248328},
+			{lat: 53.356711, lng: -6.380336}
+		]};
+
+		io.route(
+			firstParam,
+			{timeout: 10000},
+			function(results){
+				var status = results.info.statuscode;
+				if(status == 0) {
+					routeController.setRouteData(results.route);
+				}
+			});
+	};
+	
+	_drawRoute();
+	
 }); 
--- public/js/map.js
+++ public/js/map.js
@@ -5,7 +5,7 @@ YUI.add('mqlite-map', function(Y){
       var options={
         elt:document.getElementById('map'),       /*ID of element on the page where you want the map added*/ 
         zoom:10,                                  /*initial zoom level of the map*/ 
-        latLng:{lat:39.743943, lng:-105.020089},  /*center of map in latitude/longitude */ 
+        latLng:{lat:53.34804, lng:-6.248328},  /*center of map in latitude/longitude */ 
         mtype:'map',                              /*map type (map)*/ 
         bestFitMargin:0,                          /*margin offset from the map viewport when applying a bestfit on shapes*/ 
         zoomOnDoubleClick:true                    /*zoom in when double-clicking on map*/ 
